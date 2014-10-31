d9ad41fd1c5f4dc1737fc670e9d2e9410ac563e8
Michiel Rop <m.rop@onehippo.com>
2014-10-31 11:39:28 +0100

    added new imageset with variaants

---
+++ site/src/main/java/org/example/beans/Customimageset.java
@@ -0,0 +1,10 @@
+package org.example.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+
+@HippoEssentialsGenerated(internalName = "customgalleryprocessor:customimageset")
+@Node(jcrType = "customgalleryprocessor:customimageset")
+public class Customimageset extends HippoGalleryImageSet {
+}
